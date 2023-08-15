daf570f6c8f6055f34b7961043bd908c0d20536d
Liza <elizaveta.markushkina@yandex.ru>
2023-08-15 11:42:07 +0300

    cropping

--- lesson.py
+++ lesson.py
@@ -2,13 +2,17 @@ from PIL import Image, ImageFilter
 
 
 def f():
-    im = Image.open('vulkanturizmkamchatka.jpg')
-    print(im.format, im.size, im.mode)
-
     outfile = 'thumbnail.png'
     im.thumbnail((100, 100))
     im.save(outfile, format='PNG')
 
 
+def cropping():
+    box = (100, 100, 400, 400)
+    reg = im.crop(box)
+    reg.save('cropping.png', 'PNG')
+
+
 if __name__ == '__main__':
-    f()
+    im = Image.open('vulkanturizmkamchatka.jpg')
+    cropping()
