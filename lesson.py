694a4832b8838088c9dd279abbddd5f6117748df
Liza <elizaveta.markushkina@yandex.ru>
2023-08-15 11:19:16 +0300

    thumbnail

--- lesson.py
+++ lesson.py
@@ -3,6 +3,9 @@ from PIL import Image, ImageFilter
 
 def f():
     im = Image.open('vulkanturizmkamchatka.jpg')
+    print(im.format, im.size, im.mode)
+    im.thumbnail((100, 100))
+    im.save('thumbnail.jpg')
     im.show()
 
 
