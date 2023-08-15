78fb130f4dc7f328cb886693fc15d240970bf795
Liza <elizaveta.markushkina@yandex.ru>
2023-08-15 11:28:12 +0300

    stepik

--- stepik.py
+++ stepik.py
@@ -1,1 +1,9 @@
+n = int(input())
+start = ''
+for i in range(1, n + 1):
+    a = (str(i) + ' ') * i
+    start += a
+    if len(start.split()) >= n:
+        print(*start.split()[:n])
+        break
 
